065e2986d7f3d6070ce8b832023dabe2391efca2
olivier villevieille <oliviervillevieille@hotmail.fr>
2021-10-26 16:45:47 +0200

    modif et ajouts fichiers

---
+++ js/index.js
@@ -0,0 +1,37 @@
+//Map
+function initMap() {
+  // Map option
+  let options = {
+    center: { lat: 47.038868, lng: -1.6408137 },
+    zoom: 13,
+  };
+  //New Map
+  map = new google.maps.Map(document.getElementById("map"), options);
+
+  //Marker
+  const marker = new google.maps.Marker({
+    position: { lat: 47.0276091, lng: -1.6335115 },
+    map: map,
+    icon: `ressources/logos/markers.png`,
+  });
+  // infowindow
+  const detailWindow = new google.maps.InfoWindow({
+    content: `<h2>La Fabri'K des Gourmandises</h2> 
+      <span class="txt-window">Traiteur et Pâtisseries</span> 
+      <p class="post-code">9 impasse du Petit Pailler
+      </p>
+      <p class="post-code">44310 St Philbert de Grand Lieu</p>,
+  `,
+  });
+  marker.addListener("mouseover", () => {
+    detailWindow.open(map, marker);
+  });
+}
+// Scroll up
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // Si on le scroll est supérieur ou égale à 200vh
+  if (this.scrollY >= 200) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);

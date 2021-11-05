2385a25678fd7f433fa2d3fee66ca997a33301bf
olivier villevieille <oliviervillevieille@hotmail.fr>
2021-11-05 14:46:04 +0100

    modid responsive & ajout loader

--- js/index.js
+++ js/index.js
@@ -1,32 +1,32 @@
 //Map
-// function initMap() {
-//   // Map option
-//   let options = {
-//     center: { lat: 47.038868, lng: -1.6408137 },
-//     zoom: 13,
-//   };
-//   //New Map
-//   map = new google.maps.Map(document.getElementById("map"), options);
+function initMap() {
+  // Map option
+  let options = {
+    center: { lat: 47.038868, lng: -1.6408137 },
+    zoom: 13,
+  };
+  //New Map
+  map = new google.maps.Map(document.getElementById("map"), options);
 
-//   //Marker
-//   const marker = new google.maps.Marker({
-//     position: { lat: 47.0276091, lng: -1.6335115 },
-//     map: map,
-//     icon: `ressources/logos/markers.png`,
-//   });
-//   // infowindow
-//   const detailWindow = new google.maps.InfoWindow({
-//     content: `<h2>La Fabri'K des Gourmandises</h2>
-//       <span class="txt-window">Traiteur et Pâtisseries</span>
-//       <p class="post-code">9 impasse du Petit Pailler
-//       </p>
-//       <p class="post-code">44310 St Philbert de Grand Lieu</p>,
-//   `,
-//   });
-//   marker.addListener("mouseover", () => {
-//     detailWindow.open(map, marker);
-//   });
-// }
+  //Marker
+  const marker = new google.maps.Marker({
+    position: { lat: 47.0276091, lng: -1.6335115 },
+    map: map,
+    icon: `ressources/logos/markers.png`,
+  });
+  // infowindow
+  const detailWindow = new google.maps.InfoWindow({
+    content: `<h2>La Fabri'K des Gourmandises</h2>
+      <span class="txt-window">Traiteur et Pâtisseries</span>
+      <p class="post-code">9 impasse du Petit Pailler
+      </p>
+      <p class="post-code">44310 St Philbert de Grand Lieu</p>,
+  `,
+  });
+  marker.addListener("mouseover", () => {
+    detailWindow.open(map, marker);
+  });
+}
 
 // Scroll up
 function scrollUp() {
@@ -42,13 +42,25 @@ const sr = ScrollReveal({
   origin: "top",
   distance: "80px",
   duration: 2500,
-  delay: 400,
+  delay: 1000,
   // reset: true
 });
 
-sr.reveal(`.titre-principal, .nav-links, .traiteur`);
+sr.reveal(`.titre-principal, .nav-links,.activite, .titre-activite, .traiteur`);
 sr.reveal(`.patisserie, .localisation, .about`, {
-  interval: 100,
+  interval: 200,
 });
 // sr.reveal(`.txt-traiteur, .parallax-traiteur`, { origin: "left" });
 // sr.reveal(`.txt-patisserie, .parallax-patisserie`, { origin: "right" });
+
+// =================Loader====================//
+const loader = document.querySelector(".loader");
+
+function load() {
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      loader.className += " hidden";
+    }, 2500);
+  });
+}
+load();
